6f308643d18ef8f45d0550320c7e3576ca88c8e6
tis <tishan@protonmail.com>
2022-01-23 16:40:07 +0530

    feat: scores from pgn

--- src/pgn.ts
+++ src/pgn.ts
@@ -19,5 +19,42 @@ const sanWord = (san: string, i: number) =>
 const stringifyScore = (score: []) =>
   score.map((e, i) => sanWord(e, i)).join(" ");
 
-export const toPGN = (pgn: PGNObject): string =>
-  stringifyPGNMeta(pgn) + "\n\n" + stringifyScore(pgn.score);
+export const toPGN = (pgnObj: PGNObject): string =>
+  stringifyPGNMeta(
+    Object.fromEntries(
+      Object.entries(pgnObj).filter(([k, _]) => k !== "score")
+    ) as PGNObject
+  ) +
+  "\n\n" +
+  stringifyScore(pgnObj.score);
+
+//
+
+const decodeMeta = (metaWord: string) => {
+  const metaEntry = metaWord
+    .replace(/[\[\]]/g, "")
+    .split('"')
+    .slice(0, 2);
+  metaEntry[0] = metaEntry[0]?.replace(/[^a-zA-Z]/g, "");
+  return metaEntry.length === 2 ? metaEntry : null;
+};
+
+const getMetas = (word: string) =>
+  word.match(/\[.+\]/g)?.map((m) => decodeMeta(m)) || [];
+
+const getScore = (word: string) =>
+  word
+    .replace(/\n/g, "")
+    .replace(/\[.+\]|{.+}|[{}]|[0-9]+\./g, "")
+    .split(" ")
+    .filter((e) => e && !["1-0", "0-1", "1/2-1/2"].includes(e));
+
+export const fromPGN = (pgn: string): PGNObject[] => {
+  const pgnGames = pgn.split(/\n1\./g);
+  const metas = pgnGames.slice(0, -1).map((w) => getMetas(w));
+  const scores = pgnGames.slice(1).map((w) => getScore(w));
+  return scores.map((score, i) => ({
+    ...Object.fromEntries((metas[i] as []) || []),
+    score,
+  })) as PGNObject[];
+};
